fb02aa1dfd37690b8d4ebb5d9ef1b5e877ee2918
yusewaki <sewayu.pc11@gmail.com>
2021-04-01 10:53:18 +0000

    [finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '784a6c045c7b3a5e6834695930103ba87beafbaadc54f67dd6f89a7868715cd0a5d45d7c7509ee70afa5e588a0a58ff7f954bd6549e7906bb1809626f5612208'
